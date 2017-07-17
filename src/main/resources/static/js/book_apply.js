af7e1202d8121beeacd9378bf17be6637095585c
dragonhht <dragon_hht@163.com>
2017-07-17 17:54:40 +0800

    申请图书退出

---
+++ src/main/resources/static/js/book_apply.js
@@ -0,0 +1,48 @@
+/**
+ * Created by huang on 17-7-17.
+ */
+/** 导航选择. */
+function selectNav(n) {
+    var links = $('#top_ul').children('li');
+    for (var i = 0; i < links.length; i++) {
+        $(links[i]).children('a').css('color', 'black');
+    }
+    $(n).css('color', 'gray');
+}
+
+/** 选择信息标签页. */
+function selectTab(n) {
+    var index = $(n).attr('index');
+    var links = $('#nav_tabs').children('li');
+    for (var i = 0; i < links.length; i++) {
+        $(links[i]).removeClass('active');
+    }
+    $(n).addClass('active');
+    if (index == 0) {
+        $('#book_now_have').hide();
+        $('#book_borrow').hide();
+        $('#notice').hide();
+        $('#book_contribution').show();
+        return;
+    }
+    if (index == 1) {
+        $('#book_contribution').hide();
+        $('#book_borrow').hide();
+        $('#notice').hide();
+        $('#book_now_have').show();
+        return;
+    }
+    if (index == 2) {
+        $('#book_contribution').hide();
+        $('#book_now_have').hide();
+        $('#notice').hide();
+        $('#book_borrow').show();
+        return;
+    }
+    if (index == 3) {
+        $('#book_contribution').hide();
+        $('#book_now_have').hide();
+        $('#book_borrow').hide();
+        $('#notice').show();
+    }
+}
