d32d8a6700859404d99266241328a13905925851
dragonhht <dragon_hht@163.com>
2017-08-11 17:16:11 +0800

    图书上传星级

--- src/main/resources/static/js/upload_book.js
+++ src/main/resources/static/js/upload_book.js
@@ -9,3 +9,16 @@ function selectNav(n) {
     }
     $(n).css('color', 'gray');
 }
+
+/** 星评. */
+function startLight(n) {
+    var index = parseInt($(n).attr('index')) + 1;
+    var a = $('#startComment').children('a');
+    for (var i = 0; i < index; i++) {
+        $($(a[i]).children('img')).attr('src', "../img/light_start.png");
+    }
+    for (var j = i; j < 5; j++) {
+        $($(a[j]).children('img')).attr('src', "../img/dark_start.png");
+    }
+    $('#bookStart').val(index);
+}
