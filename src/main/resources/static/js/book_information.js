fa2c05e8788de05896f6e5dd7ebe34d67dc64f1b
dragonhht <dragon_hht@163.com>
2017-10-14 15:27:25 +0800

    图书详细信息

---
+++ src/main/resources/static/js/book_information.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+
+    /** 借阅申请. */
+    $('#applyBtn').click(function () {
+        var height = $(document).height();
+        var width = $(document).width();
+        var divWidth = $('#wantLook').width();
+        $('#out_bg').css('height', height);
+        $('#wantLook').css('left', (width - divWidth)/2 + "px");
+        $('#out_bg').show();
+        $('#wantLook').show();
+    });
+
+    $('#out_bg').click(function () {
+        $('#wantLook').hide();
+        $('#out_bg').hide();
+    });
+
+    /** 关闭聊天框. */
+    $('#close').click(function () {
+        var parent = $('#close').parents('#wantLook');
+        $(parent).hide();
+        $('#out_bg').hide();
+    });
+
+    /** 提交图书传阅申请. */
+    $('#updateImgBtn').click(function () {
+        var bookId = $('#bookId').val().trim();
+        var toUserId = $('#toUserId').val().trim();
+        var wantSay = $('#wantSay').val().trim();
+        $.post('../../user/wantBook',
+            {
+                bookId : bookId,
+                toUserId : toUserId,
+                wantSay : wantSay
+            },
+            function (data) {
+                if (data == 'ok') {
+                    $('.alert').html('提交成功').addClass('alert-success').show().delay(2000).fadeOut();
+                    $('#wantLook').hide();
+                    $('#out_bg').hide();
+                } else {
+                    $('.alert').html('提交失败').addClass('alert-danger').show().delay(2000).fadeOut();
+                    $('#wantLook').hide();
+                    $('#out_bg').hide();
+                }
+            })
+    });
+
+});
